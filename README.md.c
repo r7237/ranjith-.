0bc32ddff830b76d082ca5bf7962f9c2427dd0a9
r7237 <35914754+r7237@users.noreply.github.com>
2018-03-28 15:46:13 +0530

    Update README.md.c

--- README.md.c
+++ README.md.c
@@ -2,7 +2,7 @@ import java.util.*;
 import java.lang.*;
 public class guvi
 {
-    public static void main(String args[])
+        public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
         
